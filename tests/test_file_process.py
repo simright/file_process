689d876e036b96131a2de5cb3546bf554f43fca5
shineyao <shineyao0221@163.com>
2017-07-04 10:37:50 +0800

    Refine test_file_process Refine the test_file_process

--- tests/test_file_process.py
+++ tests/test_file_process.py
@@ -1,7 +1,5 @@
 import unittest
 import os
-import shutil
-import filecmp
 
 from file_process.master_file import AllFile
 
@@ -13,52 +11,44 @@ class TestFileProcess(unittest.TestCase):
     def get_model_path(self, fname):
         return os.path.join(self.fullpath_dir_of_models, fname)
 
-    def test_io(self, overwrite=False):
+    def test_io(self):
         print 'Testing File Process ...'
 
-        fname = [
-                "includes/nodes.k",
-                "includes/elements.k",
-                "elements.fem",
-                "elements.inp",
-                "quad.fem",
-                "quad.bdf",
-                "grids.bdf",
-                "nodes.inp",
-                "elements.bdf",
-                "quad.k",
-                "grids.fem",
-                "quad.inp",
-                ]
-        list_file_path = list()
+        all_fname = [
+                    "includes/nodes.k",
+                    "includes/elements.k",
+                    "elements.fem",
+                    "elements.inp",
+                    "quad.fem",
+                    "quad.bdf",
+                    "grids.bdf",
+                    "nodes.inp",
+                    "elements.bdf",
+                    "quad.k",
+                    "grids.fem",
+                    "quad.inp",
+                    ]
+        master_fname = [
+                    "quad.fem",
+                    "quad.bdf",
+                    "quad.k",
+                    "quad.inp",
+                       ]
+        list_all_file_path = list()
+        list_master_file_path = list()
 
-        for filename in fname:
-            list_file_path.append(self.get_model_path(filename))
+        for filename in master_fname:
+            list_master_file_path.append(self.get_model_path(filename))
+        for filename in all_fname:
+            list_all_file_path.append(self.get_model_path(filename))
 
         all_file = AllFile()
-        master_file_path = all_file.find_master_file(list_file_path)
+        found_master_file_path = all_file.find_master_file(list_all_file_path)
 
-        fpath_out_ref = self.get_model_path('master_file_path.ref')
-        fpath_out_new = fpath_out_ref[:-4] + '.new'
-        fo = open(fpath_out_new, 'w')
-        for line in master_file_path:
-            fo.write(line + '\n')
-        fo.close()
+        for path in found_master_file_path:
+            assert path in list_master_file_path
+        assert len(found_master_file_path) == len(master_fname)
+        print 'Test passed'
 
-        self.assertTrue(os.path.isfile(fpath_out_new))
-        if not os.path.isfile(fpath_out_ref):
-            print 'WARNING: REF file not exist!'
-            shutil.copyfile(fpath_out_new, fpath_out_ref)
-            print '  COPYing: %s' % fpath_out_new
-            print '  To     : %s' % fpath_out_ref
-
-        if filecmp.cmp(fpath_out_ref, fpath_out_new, shallow=False):
-            os.remove(fpath_out_new)
-        else:
-            if overwrite:
-                shutil.move(fpath_out_new, fpath_out_ref)
-            else:
-                self.fail('Diffs found in:\n' + '  ' + fpath_out_ref + '\n' + '  ' + fpath_out_new)
-
-if __name__ =='__main__':
+if __name__ == '__main__':
     unittest.main()
