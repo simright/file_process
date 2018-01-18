7060eab1108021897c5b4336fea422344090e59f
woscj607 <woscj607@163.com>
2018-01-18 11:23:12 +0800

    only keyword "GRID" is not enough, add some other keywords to check if it is nastran file or not, the content below statement "if line_lower.startswith('include')" is modefied to "return True"

--- fem_utils/nastran_utils.py
+++ fem_utils/nastran_utils.py
@@ -1,4 +1,4 @@
-import os
+import os, re
 
 from fem_utils import common_utils
 
@@ -39,29 +39,23 @@ def is_nastran_file(fpath):
     return is_nastran_file_by_content(fpath)
 
 
+def nastran_keywords_pattern():
+    keywords_pattern = r'^(grid)|(ctria3)|(cquad4)|(chexa)|(ctera)|(mat\d)|(spc\d?)|(force\d?)|(pload\d?)'
+    return keywords_pattern
+
+
 def is_nastran_file_by_content(fpath):
     with open(fpath, 'r+') as fp:
         f_content_line_list = fp.readlines()
         for line in f_content_line_list:
-            if line.startswith('GRID') or line.startswith('grid'):
+            line_lower = line.strip().lower()
+            if re.search(nastran_keywords_pattern(), line_lower):
                 return True
-            if line.startswith('INCLUDE') or line.startswith('include'):
-                line = line.strip()
-                wordsInLine_list = line.split()
-                if len(wordsInLine_list) > 1:
-                    include_fname = wordsInLine_list[1]
-                    include_fname = include_fname.strip('"')
-                    include_fname = include_fname.strip("'")
-                    include_fpath = os.path.join(os.path.dirname(fpath), include_fname)
-                    if os.path.isfile(include_fpath):
-                        if is_nastran_file_by_content(include_fpath):
-                            return True
-    return False
-
-def nastran_keywords():
-    return ['GRID']
+            if line_lower.startswith('include'):
+                return True
+        return False
 
 
 if __name__ == "__main__":
-    fpath = 'E:/Works/GitProject/fem_utils/tests/data/nastran(.bdf)/quad_no_ext'
+    fpath = 'D:/worksCode/fem_utils/tests/data/nastran(.bdf)/quad_no_ext'
     print(is_nastran_file_by_content(fpath))
