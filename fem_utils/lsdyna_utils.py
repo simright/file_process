333f8d0bd0b20a7bc2b2fdd2f946a90f1b9a7d59
woscj607 <woscj607@163.com>
2018-01-18 11:27:11 +0800

    added more keywords to check the file is lsdyna or not, changed the content while "*include" in line of text

--- fem_utils/lsdyna_utils.py
+++ fem_utils/lsdyna_utils.py
@@ -43,33 +43,36 @@ def is_lsdyna_file(fpath):
     return is_lsdyna_file_by_content(fpath)
 
 
+def lsdyna_keywords_pattern():
+    keywords_pattern = r'^(\*node_)|(\*element_)|(\*mat_)|(\*boundary_)|(\*include_)|(\*define_)|(\*contact_)|(\*set_)'
+    return keywords_pattern
+
+
 def is_lsdyna_file_by_content(fpath):
     with open(fpath, 'r+') as fp:
         f_content_line_list = fp.readlines()
         for line in f_content_line_list:
-            if line.startswith('*NODE') or line.startswith('*node'):
-                # compare the following five lines, because ls-dyna file also has the same *node keyword
+            line_lower = line.strip().lower()
+            if re.search(r'\*keyword', line_lower):
+                return True
+            if re.search(r'^(\*node)|(\*element)', line_lower):
+                # compare the following lines, because ls-dyna file also has the same *node keyword
                 index = f_content_line_list.index(line)
                 next_ind = index + 1
-                while next_ind <= len(f_content_line_list) and f_content_line_list[next_ind].startswith('$'):
+                if next_ind >= len(f_content_line_list):
+                    return False
+                while f_content_line_list[next_ind].strip().startswith('$'):
                     next_ind += 1
                 next_line = f_content_line_list[next_ind].strip()
                 if re.match(r'^\d+', next_line) and not re.search(r',', next_line):
                     return True
-            if line.startswith('*INCLUDE') or line.startswith('*include'):
-                index = f_content_line_list.index(line)
-                next_ind = index + 1
-                while next_ind <= len(f_content_line_list) and f_content_line_list[next_ind].startswith('$'):
-                    next_ind += 1
-                next_line = f_content_line_list[next_ind].strip()
-                include_fname = next_line
-                include_fpath = os.path.join(os.path.dirname(fpath), include_fname)
-                if os.path.isfile(include_fpath):
-                    if is_lsdyna_file_by_content(include_fpath):
-                        return True
-    return False
+            if re.search(lsdyna_keywords_pattern(), line_lower):
+                return True
+            if line_lower.lower() == '*include':
+                return True
+        return False
 
 
 if __name__ == "__main__":
-    fpath = 'E:/Works/GitProject/fem_utils/tests/data/dyna(.k)/quad_no_ext'
+    fpath = 'D:/worksCode/fem_utils/tests/data/dyna(.k)/nodes_no_ext'
     print(is_lsdyna_file(fpath))
